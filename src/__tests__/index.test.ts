503f7d1d8861e01bc9790003c673629526db26b3
Tim Johns <timjohns@yahoo.com>
2020-11-25 10:08:21 -0800

    Initial checkin for testing

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,146 @@
+import { CheckPhish } from '../index';
+import { AxiosInstance } from "axios";
+
+const fakeAPIKey = "Not really an API key";
+
+describe('with defaults', () => {
+  describe('scan', () => {
+    test('uses the full default URL', async () => {
+      const mockAxios = jest.fn();
+      const axios = mockAxios as unknown as AxiosInstance;
+      const defaultOptions = { axios };
+      const checkphish = new CheckPhish(fakeAPIKey, defaultOptions);
+      const scanResponse = {
+        data: {}
+      };
+
+      mockAxios.mockResolvedValue(scanResponse);
+
+      await checkphish.scan('https://example.com');
+
+      const axiosArgs = mockAxios.mock.calls[0][0];
+
+      expect(axiosArgs.url).toEqual('https://developers.checkphish.ai/api/neo/scan');
+
+    });
+    test('has the package.json version in the default UA string', async () => {
+      const mockAxios = jest.fn();
+      const axios = mockAxios as unknown as AxiosInstance;
+      const defaultOptions = { axios };
+      const checkphish = new CheckPhish(fakeAPIKey, defaultOptions);
+      const scanResponse = {
+        data: {}
+      };
+      const { version } = require('root-require')('./package.json');
+
+      mockAxios.mockResolvedValue(scanResponse);
+
+      await checkphish.scan('https://example.com');
+
+      const axiosArgs = mockAxios.mock.calls[0][0];
+
+      expect(axiosArgs.headers['User-Agent']).toEqual(expect.stringContaining('/' + version));
+
+    });
+  });
+  describe('status', () => {
+    test('uses the full default URL', async () => {
+      const mockAxios = jest.fn();
+      const axios = mockAxios as unknown as AxiosInstance;
+      const defaultOptions = { axios };
+      const checkphish = new CheckPhish(fakeAPIKey, defaultOptions);
+      const statusResponse = {
+        data: {}
+      };
+
+      mockAxios.mockResolvedValue(statusResponse);
+
+      await checkphish.status('jobID');
+
+      const axiosArgs = mockAxios.mock.calls[0][0];
+
+      expect(axiosArgs.url).toEqual('https://developers.checkphish.ai/api/neo/status');
+
+    });
+  });
+});
+
+describe('with options', () => {
+  describe('scan', () => {
+    test('uses the specified root URL', async () => {
+      const mockAxios = jest.fn();
+      const axios = mockAxios as unknown as AxiosInstance;
+      const options = {
+        axios,
+        apiURL: "https://tenant.bolster.ai"
+      };
+      const checkphish = new CheckPhish(fakeAPIKey, options);
+      const scanResponse = {
+        data: {}
+      };
+
+      mockAxios.mockResolvedValue(scanResponse);
+
+      await checkphish.scan('https://example.com');
+
+      const axiosArgs = mockAxios.mock.calls[0][0];
+
+      expect(axiosArgs.url).toEqual('https://tenant.bolster.ai/neo/scan');
+
+    });
+    test('uses the specified UA string', async () => {
+      const mockAxios = jest.fn();
+      const axios = mockAxios as unknown as AxiosInstance;
+      const options = {
+        axios,
+        userAgent: "Just some jest tests."
+      };
+      const checkphish = new CheckPhish(fakeAPIKey, options);
+      const scanResponse = {
+        data: {}
+      };
+
+      mockAxios.mockResolvedValue(scanResponse);
+
+      await checkphish.scan('https://example.com');
+
+      const axiosArgs = mockAxios.mock.calls[0][0];
+
+      expect(axiosArgs.headers['User-Agent']).toEqual("Just some jest tests.");
+
+    });
+
+  });
+  describe('status', () => {
+    test('uses the specified root URL', async () => {
+      const mockAxios = jest.fn();
+      const axios = mockAxios as unknown as AxiosInstance;
+      const options = {
+        axios,
+        apiURL: "https://tenant.bolster.ai"
+      };
+      const checkphish = new CheckPhish(fakeAPIKey, options);
+      const statusResponse = {
+        data: {}
+      };
+
+      mockAxios.mockResolvedValue(statusResponse);
+
+      await checkphish.status('jobID');
+
+      const axiosArgs = mockAxios.mock.calls[0][0];
+
+      expect(axiosArgs.url).toEqual('https://tenant.bolster.ai/neo/status');
+
+    });
+  });
+
+});
+
+// TODO(tjohns): Test that the version number in the UA string matches package.json
+
+// TODO(tjohns): Test scan with axios throw
+// TODO(tjohns): Test scan with error response
+// TODO(tjohns): Test status with axios throw
+// TODO(tjohns): Test status with error response
+
---
+++ src/index.ts
@@ -0,0 +1,103 @@
+import defaultAxios, { AxiosInstance } from "axios";
+
+export const DEFAULT_API_URL = "https://developers.checkphish.ai/api";
+export const DEFAULT_USER_AGENT = "CheckPhishNodeLib/1.0.0";
+
+
+// TODO(tjohns): Fill in additional CheckPhishStatus fields
+// TODO(tjohns): Document CheckPhishStatus fields
+// TODO(tjohns): Determine if we can have an 'either-or' type here (i.e. can be a CheckPhishScanResponse or a CheckPhishScanErrorResponse)
+export interface CheckPhishScanResponse {
+  errorMessage?: string,
+  jobID?: string
+};
+
+// TODO(tjohns): Fill in CheckPhishStatusResponse
+export interface CheckPhishStatusResponse {
+  errorMessage?: string,
+  status?: string,
+  disposition?: string,
+  resolved?: boolean,
+  url?: string,
+  insights?: string,
+  screenshot_path?: string
+};
+
+export interface CheckPhishOptions {
+  apiURL?: string,
+  userAgent?: string,
+  axios?: AxiosInstance
+}
+
+export class CheckPhish {
+  private apiKey: string;
+  private apiURL: string;
+  private userAgent: string;
+  private axios: AxiosInstance;
+  constructor(apiKey: string, options: CheckPhishOptions = {}) {
+    this.apiKey = apiKey;
+    this.apiURL = options.apiURL || DEFAULT_API_URL;
+    this.userAgent = options.userAgent || DEFAULT_USER_AGENT;
+    this.axios = options.axios || defaultAxios;
+  }
+
+  // TODO(tjohns): Add optional parameters
+  async scan(url: string): Promise<CheckPhishScanResponse> {
+    const axios = this.axios;
+    const apiKey = this.apiKey;
+    const apiURL = this.apiURL;
+    const userAgent = this.userAgent;
+
+    const { data: scanResponse }: { data: CheckPhishScanResponse}  = await axios(
+      {
+        method: 'post',
+        url: `${apiURL}/neo/scan`,
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': userAgent
+        },
+        data: {
+          apiKey,
+          urlInfo: {
+            url
+          }
+        }
+      });
+
+    if (scanResponse.errorMessage) {
+      throw new Error(`Error scanning ${url}: ${scanResponse.errorMessage}`);
+    } else {
+      return scanResponse;
+    }
+  };
+
+
+  // TODO(tjohns): Add optional parameters (insights, et. al.)
+  async status(jobID: string, insights = true): Promise<CheckPhishStatusResponse> {
+    const axios = this.axios;
+    const apiKey = this.apiKey;
+    const apiURL = this.apiURL;
+    const userAgent = this.userAgent;
+
+    const { data: statusResponse }: { data: CheckPhishStatusResponse}  = await axios(
+      {
+        method: 'post',
+        url: `${apiURL}/neo/status`,
+        headers: {
+          'Content-Type': 'application/json',
+          'User-Agent': userAgent
+        },
+        data: {
+          apiKey,
+          jobID,
+          insights
+        }
+      });
+
+    if (statusResponse.errorMessage) {
+      throw new Error(`Error getting status for jobID ${jobID}: ${statusResponse.errorMessage}`);
+    } else {
+      return statusResponse;
+    }
+  }
+}
